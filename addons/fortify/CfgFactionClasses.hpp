095a41ebbd51a356ddd39cd8263ee24f869ef54a
Walthzer <s.v.feitsma@gmail.com>
2022-02-25 00:17:03 +0100

    Remove Tabs

--- addons/fortify/CfgFactionClasses.hpp
+++ addons/fortify/CfgFactionClasses.hpp
@@ -1,8 +1,8 @@
 class CfgFactionClasses
 {
-	class NO_CATEGORY;
-	class GVAR(moduleCategory): NO_CATEGORY
-	{
-		displayName = "@WFAR Fortify";
-	};
+    class NO_CATEGORY;
+    class GVAR(moduleCategory): NO_CATEGORY
+    {
+        displayName = "@WFAR Fortify";
+    };
 };
