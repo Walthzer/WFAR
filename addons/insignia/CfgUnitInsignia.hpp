2c0c5340f41906a4bc319d98bbf50c8b5cb894c1
Walthzer <s.v.feitsma@gmail.com>
2022-05-04 01:16:14 +0200

    fix

--- addons/insignia/CfgUnitInsignia.hpp
+++ addons/insignia/CfgUnitInsignia.hpp
@@ -2,9 +2,5 @@ class CfgUnitInsignia
 {
     #include "players\insignia.hpp"
     #include "squad\insignia.hpp"
-<<<<<<< Updated upstream
-	#include "respect\insignia.hpp"
-=======
     #include "respect\insignia.hpp"
->>>>>>> Stashed changes
 };
