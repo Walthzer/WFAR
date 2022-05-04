c92f38a960eb8eeb109bbd6ad7fa6f98d51b4adb
Walthzer <s.v.feitsma@gmail.com>
2022-05-04 23:10:27 +0200

    Update Gecko

--- addons/retextures/CfgWeapons.hpp
+++ addons/retextures/CfgWeapons.hpp
@@ -12,7 +12,7 @@ class cfgWeapons {
     class rhsusf_mich_helmet_marpatwd_norotos_arc;
     class rhsusf_mich_helmet_marpatwd_norotos_arc_headset;
 
-    //#include "data\rhsusf\mich_covered\jews\CfgWeapons.hpp"
+    #include "data\rhsusf\mich_covered\jews\CfgWeapons.hpp"
     #include "data\rhsusf\mich_covered\shark\CfgWeapons.hpp"
 
     class rhsusf_cvc_alt_helmet;
