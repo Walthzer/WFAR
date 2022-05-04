c5bcf7ec47118686439bf4c26dd48b43d5bdc3c2
Walthzer <s.v.feitsma@gmail.com>
2022-05-04 17:51:24 +0200

    back to relative paths

--- addons/retextures/CfgWFARArsenalAccess.hpp
+++ addons/retextures/CfgWFARArsenalAccess.hpp
@@ -1,20 +1,25 @@
-#define FINAL
 class cfgWFARArsenalAccess
 {
     class IDX_VIRT_HEADGEAR
     {
          //Jews
         76561198042318568[]={
-            #include "\z\wfar\addons\retextures\data\rhsusf\mich_covered\jews\weapons.hpp"
+            #define FINAL
+            #include "data\rhsusf\mich_covered\jews\weapons.hpp"
+            #undef FINAL
         };
         //Shark
         76561198053950415[]={
-            #include "\z\wfar\addons\retextures\data\rhsusf\mich_covered\jews\weapons.hpp"
-            #include "\z\wfar\addons\retextures\data\rhsusf\cvc\gecko\weapons.hpp"
+            #include "data\rhsusf\mich_covered\jews\weapons.hpp"
+            #define FINAL
+            #include "data\rhsusf\cvc\gecko\weapons.hpp"
+            #undef FINAL
         };
         //Brenn
         76561198118410862[]={
-            #include "\z\wfar\addons\retextures\data\rhsusf\cvc\gecko\weapons.hpp"
+            #define FINAL
+            #include "data\rhsusf\cvc\gecko\weapons.hpp"
+            #undef FINAL
         };
-    }
+    };
 };
