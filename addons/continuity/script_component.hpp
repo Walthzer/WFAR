dfb7cfaa127039df0e9d1ede4494f1547915523f
Walthzer <s.v.feitsma@gmail.com>
2022-05-07 15:51:36 +0200

    initial comment

---
+++ addons/continuity/script_component.hpp
@@ -0,0 +1,18 @@
+#define COMPONENT continuity
+#define COMPONENT_BEAUTIFIED Continuity
+#include "\z\wfar\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_CONTINUITY
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_CONTINUITY
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_CONTINUITY
+#endif
+
+#include "\z\wfar\addons\main\script_macros.hpp"
