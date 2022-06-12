58c8b110d5235b30cb9fa33e8d8e3bc024514648
Walthzer <s.v.feitsma@gmail.com>
2022-06-12 08:24:07 +0200

    give libby acces

--- addons/retextures/CfgWFARArsenalAccess.hpp
+++ addons/retextures/CfgWFARArsenalAccess.hpp
@@ -8,10 +8,15 @@ class cfgWFARArsenalAccess
             #include "data\rhsusf\mich_covered\jews\weapons.hpp"
             #undef FINAL
         };
+        //Libby
+        76561198453933280[]={
+            #define FINAL
+            #include "data\rhsusf\mich_covered\libby\weapons.hpp"
+            #undef FINAL
+        };
         //Shark
         76561198053950415[]={
             #include "data\rhsusf\mich_covered\shark\weapons.hpp"
-            #include "data\rhsusf\mich_covered\libby\weapons.hpp"
             #define FINAL
             #include "data\rhsusf\cvc\gecko\weapons.hpp"
             #undef FINAL
