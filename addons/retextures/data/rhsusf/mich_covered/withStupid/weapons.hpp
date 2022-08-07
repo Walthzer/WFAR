d6ae4e18d74c97a07a3f58c038e6a156038b2228
Walthzer <s.v.feitsma@gmail.com>
2022-08-07 02:54:43 +0200

    add withStupid

---
+++ addons/retextures/data/rhsusf/mich_covered/withStupid/weapons.hpp
@@ -0,0 +1,16 @@
+"wfar_withStupid_mich_helmet_marpatwd",
+"wfar_withStupid_mich_helmet_marpatd",
+"wfar_withStupid_mich_helmet_marpatwd_headset",
+"wfar_withStupid_mich_helmet_marpatd_headset",
+"wfar_withStupid_mich_helmet_marpatwd_alt",
+"wfar_withStupid_mich_helmet_marpatd_alt",
+"wfar_withStupid_mich_helmet_marpatwd_alt_headset",
+"wfar_withStupid_mich_helmet_marpatd_alt_headset",
+"wfar_withStupid_mich_helmet_marpatwd_norotos",
+"wfar_withStupid_mich_helmet_marpatd_norotos",
+"wfar_withStupid_mich_helmet_marpatwd_norotos_arc",
+#ifdef FINAL
+    "wfar_withStupid_mich_helmet_marpatd_norotos_arc"
+#else
+    "wfar_withStupid_mich_helmet_marpatd_norotos_arc",
+#endif
