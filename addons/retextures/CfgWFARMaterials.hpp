a2e6837a0dedd0afedff76560c4aca4589ed0a17
Walthzer <19926813+Walthzer@users.noreply.github.com>
2025-03-28 15:31:32 +0100

    massive push

---
+++ addons/retextures/CfgWFARMaterials.hpp
@@ -0,0 +1,31 @@
+class wfar_retextures_materials {
+    slots = "20";
+    class helmet {
+        class cup {
+            class ech {
+                class bases {
+                    class marpat_wd {
+                        displayName = "MARPAT Woodland";
+                        access = QUOTE(true);
+                        class layers {
+                            class 0 {
+                                path = "";
+                            };
+                        };
+                    };
+                };
+                class materials {
+                    class barker {
+                        displayName = "Barker";
+                        steamIDList[] = {76561198158672135};
+                        class layers {
+                            class 1 {
+                                path = "";
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+};
--- addons/retextures/ui/RscDisplayMaterial.hpp
+++ addons/retextures/ui/RscDisplayMaterial.hpp
@@ -1,5 +1,3 @@
-#include "defines.hpp"
-
 class RscPictureKeepAspect;
 class GVAR(RscDisplayMaterial)
 {
@@ -8,73 +6,23 @@ class GVAR(RscDisplayMaterial)
     {
         class layer0: RscPictureKeepAspect 
         {
-            idc = IDCMateriallayer0;
-            text = "";
+            idc = 0;
+            deletable=1;
+            text = "#(rgb,8,8,3)color(1,0,0,1)";
             x=0;
             y=0;
             w=1;
             h=1;
         };
-        class layer1: layer0 
+        class layer1: layer0
         {
-            idc = IDCMaterialLayer1;
+            idc = 0;
             text = "";
         };
-        class layer2: layer0 
+        class layer2: layer0
         {
-            idc = IDCMaterialLayer2;
+            idc = 0;
             text = "";
         };
-        class layer3: layer0 
-        {
-            idc = IDCMaterialLayer3;
-            text = "";
-        };
-        class layer4: layer0 
-        {
-            idc = IDCMaterialLayer4;
-            text = "";
-        };
-        class layer5: layer0 
-        {
-            idc = IDCMaterialLayer5;
-            text = "";
-        };
-        class layer6: layer0 
-        {
-            idc = IDCMaterialLayer6;
-            text = "";
-        };
-        class layer7: layer0 
-        {
-            idc = IDCMaterialLayer7;
-            text = "";
-        };
-        class layer8: layer0 
-        {
-            idc = IDCMaterialLayer8;
-            text = "";
-        };
-        class layer9: layer0 
-        {
-            idc = IDCMaterialLayer9;
-            text = "";
-        };
-        class layer10: layer0 
-        {
-            idc = IDCMaterialLayer10;
-            text = "";
-        };
-        class layer11: layer0 
-        {
-            idc = IDCMaterialLayer11;
-            text = "";
-        };
-        class layer12: layer0 
-        {
-            idc = IDCMaterialLayer12;
-            text = "";
-        };
-        
     };
 };
